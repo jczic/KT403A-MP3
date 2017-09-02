cc072d13e9237e24a6ba58a746f7d6c2a107a04f
jczic <jczic@free.fr>
2017-09-03 01:41:47 +0200

    update

--- kt403A.py
+++ kt403A.py
@@ -1,6 +1,7 @@
 
-from    machine import UART
-from    utime   import sleep_ms
+from machine import UART
+from utime   import sleep_ms
+from ustruct import unpack
 
 class KT403A :
 
@@ -8,26 +9,43 @@ class KT403A :
     # ===( Constants )============================================================
     # ============================================================================
 
-    DEVICE_MICRO_SD = 0x02
+    DEVICE_U_DISK = 1
+    DEVICE_SD     = 2
+    DEVICE_AUX    = 3
+    DEVICE_SLEEP  = 4
+    DEVICE_FLASH  = 5
 
-    EQ_NORMAL       = 0
-    EQ_POP          = 1
-    EQ_ROCK         = 2
-    EQ_JAZZ         = 3
-    EQ_CLASSIC      = 4
-    EQ_BASS         = 5
+    EQ_NORMAL     = 0
+    EQ_POP        = 1
+    EQ_ROCK       = 2
+    EQ_JAZZ       = 3
+    EQ_CLASSIC    = 4
+    EQ_BASS       = 5
 
     # ============================================================================
     # ===( Constructor )==========================================================
     # ============================================================================
 
-    def __init__(self, uartBus, txPinNum, rxPinNum, device=None) :
+    def __init__( self,
+                  uartBus,
+                  txPinNum,
+                  rxPinNum,
+                  device    = None,
+                  volume    = 70,
+                  eq        = None ) :
         txPinId = "P" + str(txPinNum)
         rxPinId = "P" + str(rxPinNum)
         self._uart = UART( uartBus,
                            baudrate = 9600,
+                           bits     = 8,
+                           parity   = None,
+                           stop     = 1,
                            pins     = (txPinId, rxPinId, None, None) )
-        self.SetDevice(KT403A.DEVICE_MICRO_SD if not device else device)
+        self.SetDevice(device if device else KT403A.DEVICE_SD)
+        if not self.GetState() :
+            raise Exception('KT403A could not be initialized.')
+        self.SetVolume(volume)
+        self.SetEqualizer(eq if eq else KT403A.EQ_NORMAL)
 
     # ============================================================================
     # ===( Utils )================================================================
@@ -42,7 +60,33 @@ class KT403A :
         self._uart.write(bytes([dataH])) # DataH
         self._uart.write(bytes([dataL])) # DataL
         self._uart.write(b'\xEF')        # Stop
-        sleep_ms(200 if cmd == 0x09 else 30)
+        sleep_ms(200 if cmd == 0x09 else 1000 if cmd == 0x0C else 30)
+
+    # ----------------------------------------------------------------------------
+
+    def _rxCmd(self) :
+        if self._uart.any() :
+            buf = self._uart.read(10)
+            if buf is not  None and \
+               len(buf) ==   10 and \
+               buf[0]   == 0x7E and \
+               buf[1]   == 0xFF and \
+               buf[2]   == 0x06 and \
+               buf[9]   == 0xEF     :
+               cmd  = buf[3]
+               data = unpack('>H', buf[5:7])[0]
+               return (cmd, data)
+        return None
+
+    # ----------------------------------------------------------------------------
+
+    def _readLastCmd(self) :
+        res = None
+        while True :
+            r = self._rxCmd()
+            if not r :
+                return res
+            res = r
 
     # ============================================================================
     # ===( Functions )============================================================
@@ -95,12 +139,18 @@ class KT403A :
     # ----------------------------------------------------------------------------
 
     def SetDevice(self, device) :
+        self._device = device
         self._txCmd(0x09, device)
 
     # ----------------------------------------------------------------------------
 
-    def SetLowPower(self) :
+    def SetLowPowerOn(self) :
         self._txCmd(0x0A)
+
+    # ----------------------------------------------------------------------------
+
+    def SetLowPowerOff(self) :
+        self.SetDevice(self._device)
 
     # ----------------------------------------------------------------------------
 
@@ -124,18 +174,114 @@ class KT403A :
 
     # ----------------------------------------------------------------------------
 
-    def StartLoopingAll(self) :
+    def EnableLoopAll(self) :
         self._txCmd(0x11, 1)
 
     # ----------------------------------------------------------------------------
 
-    def StopLoopingAll(self) :
+    def DisableLoopAll(self) :
         self._txCmd(0x11, 0)
+
+    # ----------------------------------------------------------------------------
+
+    def PlayFolder(self, folderIndex) :
+        self._txCmd(0x12, folderIndex)
 
     # ----------------------------------------------------------------------------
 
     def Stop(self) :
         self._txCmd(0x16)
+
+    # ----------------------------------------------------------------------------
+
+    def LoopFolder(self, folderIndex) :
+        self._txCmd(0x17, folderIndex)
+
+    # ----------------------------------------------------------------------------
+
+    def RandomAll(self) :
+        self._txCmd(0x18)
+
+    # ----------------------------------------------------------------------------
+
+    def EnableLoop(self) :
+        self._txCmd(0x19, 0)
+
+    # ----------------------------------------------------------------------------
+
+    def DisableLoop(self) :
+        self._txCmd(0x19, 1)
+
+    # ----------------------------------------------------------------------------
+
+    def EnableDAC(self) :
+        self._txCmd(0x1A, 0)
+
+    # ----------------------------------------------------------------------------
+
+    def DisableDAC(self) :
+        self._txCmd(0x1A, 1)
+
+    # ----------------------------------------------------------------------------
+
+    def GetState(self) :
+        self._txCmd(0x42)
+        r = self._readLastCmd()
+        return r[1] if r and r[0] == 0x42 else None
+
+    # ----------------------------------------------------------------------------
+
+    def GetVolume(self) :
+        self._txCmd(0x43)
+        r = self._readLastCmd()
+        return int(r[1] / 0x1E *100) if r and r[0] == 0x43 else 0
+
+    # ----------------------------------------------------------------------------
+
+    def GetEqualizer(self) :
+        self._txCmd(0x44)
+        r = self._readLastCmd()
+        return r[1] if r and r[0] == 0x44 else 0
+
+    # ----------------------------------------------------------------------------
+
+    def GetFilesCount(self, device=None) :
+        if not device :
+            device = self._device
+        if device == KT403A.DEVICE_U_DISK :
+            self._txCmd(0x47)
+        elif device == KT403A.DEVICE_SD :
+            self._txCmd(0x48)
+        elif device == KT403A.DEVICE_FLASH :
+            self._txCmd(0x49)
+        else :
+            return 0
+        sleep_ms(200)
+        r = self._readLastCmd()
+        return r[1] if r and r[0] >= 0x47 and r[0] <= 0x49 else 0
+
+    # ----------------------------------------------------------------------------
+
+    def GetFolderFilesCount(self, folderIndex) :
+        self._txCmd(0x4E, folderIndex)
+        sleep_ms(200)
+        r = self._readLastCmd()
+        return r[1] if r and r[0] == 0x4E else 0
+
+    # ----------------------------------------------------------------------------
+
+    def IsStopped(self) :
+        return self.GetState() == 0x0200
+
+    # ----------------------------------------------------------------------------
+
+    def IsPlaying(self) :
+        return self.GetState() == 0x0201
+
+    # ----------------------------------------------------------------------------
+
+    def IsPaused(self) :
+        return self.GetState() == 0x0202
 
     # ============================================================================
     # ============================================================================
--- main.py
+++ main.py
@@ -6,5 +6,4 @@ print(" Test KT403A MP3 ")
 print("-----------------")
 
 mp3 = KT403A(1, 3, 4)
-mp3.SetVolume(70)
-mp3.StartLoopingAll()
+mp3.EnableLoopAll()
